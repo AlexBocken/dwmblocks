113fd0e28fa1224c915538961cf123e8f5ba8d59
Alexander Bocken <alexander@bocken.org>
2020-09-21 22:06:43 +0200

    added dunst status to statusbar

--- config.h
+++ config.h
@@ -5,12 +5,13 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"music", 10, 11},
 	{"cat /tmp/mucc_time 2> /dev/null", 100, 3},
+	{"dunststatus", 10, 5},
 	{"getpomminutesft", 100, 3},
 	{"getpomminutes", 100, 3},
 /*	{"weather", 3600, 5},*/
 	{"pacpackages", 100, 0},
 	{"cpu", 2, 0},
-	// {"nettraf", 10, 0},
+	{"nettraf", 5, 0},
 	{"torrent", 6, 0},
 	{"getmail", 20, 13},
 	{"news", 600, 13},
